6a65805d65733f87cbb444dd9155e3d682f9d94e
sdgamboa <samuel.gamboa.tuz@gmail.com>
2023-10-13 14:04:49 -0400

    upload script for running propagation

---
+++ method2/runPropagation.sh
@@ -0,0 +1,44 @@
+#! /usr/bin/bash
+
+physiologies=(
+    'aerophilicity'
+#   'antimicrobial resitance'
+#    'growth temperature'
+#    'disease association'
+#    'biosafety level'
+#    'gram stain'
+#    'shape'
+#    'motility'
+#    'arrangement'
+#    'coding genes'
+#    'genome size'
+#    'extreme environment'
+#    'spore formation'
+#    'animal pathogen'
+#    'plant pathogenicity'
+#    'optimal ph'
+#    'width'
+#    'COGEM pathogenicity rating'
+#    'mutation rate per site per year'
+#    'antimicrobial sensitivity'
+#    'pathogenicity human'
+#    'hemolysis'
+#    'length'
+#    'biofilm forming'
+#    'growth medium'
+#    'mutation rate per site per generation'
+#    'spore shape'
+#    'health associated'
+#    'sphingolipid producing'
+#    'acetate producing'
+#    'butyrate producing'
+#    'lactate producing'
+#    'hydrogen gas producing'
+)
+
+for i in "${physiologies[@]}"
+do
+    echo "generateing data for $i"
+    /usr/local/bin/Rscript generate_data.R "$i" $1
+done
+
