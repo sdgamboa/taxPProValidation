f9b77c81b62f3b9eef4495e06bc0c34c2778e013
sdgamboa <samuel.gamboa.tuz@gmail.com>
2023-11-03 17:41:36 -0400

    update table with mcc summaries

---
+++ method2/validation_tables/read_tables.R
@@ -0,0 +1,11 @@
+library(purrr)
+library(readr)
+library(dplyr)
+
+fileNames <- list.files(
+    'method2/validation_tables/', pattern = "mcc", full.names = TRUE
+)
+
+dat <- map(fileNames, ~ read_tsv(.x, show_col_types = FALSE)) |> 
+    bind_rows()
+write_tsv(x = dat, file = 'method2/validation_tables/merged_data.tsv')
