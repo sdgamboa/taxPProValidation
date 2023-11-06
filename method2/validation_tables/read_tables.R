995d24269943c6bbdc364b98ab5e162b64cd781b
sdgamboa <samuel.gamboa.tuz@gmail.com>
2023-11-06 16:02:19 -0500

    add tables

--- method2/validation_tables/read_tables.R
+++ method2/validation_tables/read_tables.R
@@ -2,6 +2,8 @@ library(purrr)
 library(readr)
 library(dplyr)
 library(tidyr)
+library(ggplot2)
+library(ggrepel)
 
 dirpath <- file.path('method2', 'validation_tables')
 
@@ -52,7 +54,31 @@ dat |>
     relocate(Physiology, Rank, Attribute, meanMCC) |> 
     View()
 
+dat |> 
+    mutate(
+        Physiology = case_when(
+            Physiology == 'COGEM_pathogenicity_rating' ~ 'COGEN_rating',
+            Physiology == 'mutation_rate_per_site_per_generation' ~ 'mut_per_year',
+            Physiology == 'mutation_rate_per_site_per_year' ~ 'mut_per_generation',
+            TRUE ~ Physiology
+            
+        )
+    ) |> 
+    ggplot(aes(meanSource, meanMCC)) +
+    geom_point(shape = 1) +
+    geom_hline(yintercept = 0.4, color = 'red') +
+    geom_text_repel(aes(label = Attribute), size = 2) +
+    facet_grid(Rank ~ Physiology) +
+    labs(
+        x = 'Number of annotations from sources (Mean of 10 Folds)',
+        y = 'Matthews Correlation Coefficient (Mean of 10 Folds)'
+    ) +
+    theme_bw() +
+    theme(
+        axis.text.x = element_text(angle = 45, hjust = 1)
+    )
 
-
-
+ggsave(
+    filename = 'mcc_vs_source.png', height = 10, width = 25, dpi = 300
+)
 
