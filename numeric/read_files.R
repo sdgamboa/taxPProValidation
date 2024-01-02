c63c38454f9cce4af9f7170aca1f46526a3b3023
sdgamboa <samuel.gamboa.tuz@gmail.com>
2024-01-02 09:44:09 -0500

    update validation summary for numeric values

--- numeric/read_files.R
+++ numeric/read_files.R
@@ -2,13 +2,14 @@ library(purrr)
 library(dplyr)
 library(readr)
 
-fnames <- list.files(path = 'numeric', pattern = '(all|genus|species|strain).*tsv', full.names = TRUE)
+fnames <- list.files(path = '.', pattern = '(all|genus|species|strain).*tsv', full.names = TRUE)
 
 dat <- map(fnames, ~ read_tsv(.x, show_col_types = FALSE) ) |> 
     bind_rows() |> 
     relocate(physiology, rank)
 
 write.table(
-    x = dat, file = 'numeric/numeric_castor-ltp.tsv', 
+    x = dat, file = 'numeric_castor-ltp_summary.tsv', 
     sep = '\t', row.names = FALSE, quote = FALSE
 )
+
