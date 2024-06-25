83519a2ee06cc7edcc7f0200efeb76250226fd57
sdgamboa <samuel.gamboa.tuz@gmail.com>
2024-06-25 16:07:00 -0400

    Remove previous attempts of ASR using picrust1 and picrust2

--- picrust2/picrust2.sh
+++
@@ -1,8 +0,0 @@
-#! /bin/bash
-
-for i in $(ls train_folds | sed -e "s/\.csv$//")
-do 
-    echo -e "Running propagation for $i"
-    hsp.py -n -t LTP_all_08_2023.newick --observed_trait_table train_folds/$i.csv -m scp -o predicted/$i.tsv --processes 1
-done
-
